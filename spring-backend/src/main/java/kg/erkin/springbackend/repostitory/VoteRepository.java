a402ab8a87aabee9862c3635911ab80fe8150ff6
Erkin <erkin1399@gmail.com>
2022-07-18 14:57:32 +0600

    Add Vote repository

--- spring-backend/src/main/java/kg/erkin/springbackend/repostitory/VoteRepository.java
+++ spring-backend/src/main/java/kg/erkin/springbackend/repostitory/VoteRepository.java
@@ -1,10 +1,14 @@
 package kg.erkin.springbackend.repostitory;
 
+import kg.erkin.springbackend.model.entity.Post;
+import kg.erkin.springbackend.model.entity.User;
 import kg.erkin.springbackend.model.entity.Vote;
 import kg.erkin.springbackend.repostitory.base.BaseRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface VoteRepository extends BaseRepository<Vote> {
-//    Optional<Vote> findTopByPostAndUserOrderOrderByVoteIdDesc(Post post, User currentUser);
+    Optional<Vote> findTopByPostAndUserOrderByVoteIdDesc(Post post, User currentUser);
 }
