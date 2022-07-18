40620a7d8309be261b66cc076d5ef51a856fa2cd
Erkin <erkin1399@gmail.com>
2022-07-18 14:59:26 +0600

    Change security keys

--- spring-backend/src/main/java/kg/erkin/springbackend/security/JwtProvider.java
+++ spring-backend/src/main/java/kg/erkin/springbackend/security/JwtProvider.java
@@ -2,73 +2,62 @@ package kg.erkin.springbackend.security;
 
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
-import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.Authentication;
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 
 import javax.annotation.PostConstruct;
-import javax.crypto.spec.SecretKeySpec;
-import javax.xml.bind.DatatypeConverter;
 import java.io.IOException;
 import java.io.InputStream;
 import java.security.*;
 import java.security.cert.CertificateException;
+import java.security.interfaces.RSAPrivateKey;
+import java.security.interfaces.RSAPublicKey;
 import java.sql.Date;
 import java.time.Instant;
 
 import static io.jsonwebtoken.Jwts.parser;
 import static java.util.Date.from;
 
-@Component
-@RequiredArgsConstructor
+@Service
 public class JwtProvider {
-    @Value("${jwt.secret.key}")
-    private String secretKey;
+
     @Value("${jwt.expiration.time}")
     private Long jwtExpirationInMillis;
 
-    //The JWT signature algorithm we will be using to sign the token
-    private SignatureAlgorithm signatureAlgorithm = SignatureAlgorithm.HS256;
+    @Value("${jwt.public.key}")
+    RSAPublicKey publicKey;
+
+    @Value("${jwt.private.key}")
+    RSAPrivateKey privateKey;
 
     public String generateToken(Authentication authentication) {
-        //We will sign our JWT with our ApiKey secret
-        byte[] apiKeySecretBytes = DatatypeConverter.parseBase64Binary(secretKey);
-        Key signingKey = new SecretKeySpec(apiKeySecretBytes, signatureAlgorithm.getJcaName());
-
         org.springframework.security.core.userdetails.User principal = (org.springframework.security.core.userdetails.User) authentication.getPrincipal();
         return Jwts.builder()
                 .setSubject(principal.getUsername())
                 .setIssuedAt(from(Instant.now()))
-                .signWith(signingKey, signatureAlgorithm)
+                .signWith(privateKey)
                 .setExpiration(Date.from(Instant.now().plusMillis(jwtExpirationInMillis)))
                 .compact();
     }
 
     public String generateTokenWithUserName(String username) {
-
-        //We will sign our JWT with our ApiKey secret
-        byte[] apiKeySecretBytes = DatatypeConverter.parseBase64Binary(secretKey);
-        Key signingKey = new SecretKeySpec(apiKeySecretBytes, signatureAlgorithm.getJcaName());
-
         return Jwts.builder()
                 .setSubject(username)
                 .setIssuedAt(from(Instant.now()))
-                .signWith(signingKey, signatureAlgorithm)
+                .signWith(privateKey)
                 .setExpiration(Date.from(Instant.now().plusMillis(jwtExpirationInMillis)))
                 .compact();
     }
 
     public boolean validateToken(String jwt) {
-        parser().setSigningKey(DatatypeConverter.parseBase64Binary(secretKey)).parseClaimsJws(jwt);
+        parser().setSigningKey(publicKey).parseClaimsJws(jwt);
         return true;
     }
 
-
     public String getUsernameFromJwt(String token) {
         Claims claims = parser()
-                .setSigningKey(DatatypeConverter.parseBase64Binary(secretKey))
+                .setSigningKey(publicKey)
                 .parseClaimsJws(token)
                 .getBody();
 
