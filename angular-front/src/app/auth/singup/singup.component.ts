3e4790331ea8770f5b7b6678b3d4431de27bddad
Erkin <erkin1399@gmail.com>
2022-07-21 18:23:37 +0600

    Change sign up component

--- angular-front/src/app/auth/singup/singup.component.ts
+++ angular-front/src/app/auth/singup/singup.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {FormControl, FormGroup, Validators} from "@angular/forms";
+import {SignupRequestPayload} from "./singup-request.payload";
+import {AuthService} from "../shared/auth.service";
 
 @Component({
   selector: 'app-singup',
@@ -6,10 +9,43 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./singup.component.css']
 })
 export class SingupComponent implements OnInit {
+  signupRequestPayload: SignupRequestPayload;
+  signupForm: FormGroup;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private authService: AuthService) {
+    this.authService = authService;
+    this.signupRequestPayload = {
+      username: '',
+      email: '',
+      password: ''
+    }
+    this.signupForm = new FormGroup({
+      username: new FormControl('', Validators.required),
+      email: new FormControl( '', [Validators.required, Validators.email]),
+      password: new FormControl( '', Validators.required)
+    });
   }
 
+  ngOnInit(): void {
+    this.signupForm = new FormGroup({
+      username: new FormControl('', Validators.required),
+      email: new FormControl( '', [Validators.required, Validators.email]),
+      password: new FormControl( '', Validators.required)
+    });
+  }
+
+
+  signup(){
+    // @ts-ignore
+    this.signupRequestPayload.email = this.signupForm.get('email').value;
+    // @ts-ignore
+    this.signupRequestPayload.username = this.signupForm.get('username').value;
+    // @ts-ignore
+    this.signupRequestPayload.password = this.signupForm.get('password').value;
+
+    this.authService.signup(this.signupRequestPayload)
+      .subscribe(data => {
+        console.log(data)
+      });
+  }
 }
