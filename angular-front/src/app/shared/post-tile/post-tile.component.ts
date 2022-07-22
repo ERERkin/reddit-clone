3ffad56f7e6226894f40eb6502f502a7001b60cc
Erkin <erkin1399@gmail.com>
2022-07-22 20:06:43 +0600

    Add post service, post-tile, side-bar, subreddit-side-bar and vote-button

---
+++ angular-front/src/app/shared/post-tile/post-tile.component.ts
@@ -0,0 +1,30 @@
+import {Component, Input, OnInit, ViewEncapsulation} from '@angular/core';
+import {PostModel} from "../post-model";
+import { faComments } from '@fortawesome/free-solid-svg-icons';
+import {Router} from "@angular/router";
+import {PostService} from "../post.service";
+
+@Component({
+  selector: 'app-post-tile',
+  templateUrl: './post-tile.component.html',
+  styleUrls: ['./post-tile.component.css'],
+  encapsulation: ViewEncapsulation.None,
+})
+export class PostTileComponent implements OnInit {
+
+  posts$: Array<PostModel> | undefined;
+  faComments = faComments;
+
+  constructor(private postService: PostService, private router: Router) {
+    this.postService.getAllPosts().subscribe(post => {
+      this.posts$ = post;
+    });
+  }
+
+  ngOnInit(): void {
+  }
+
+  goToPost(id: number): void {
+    this.router.navigateByUrl('/view-post/' + id);
+  }
+}
