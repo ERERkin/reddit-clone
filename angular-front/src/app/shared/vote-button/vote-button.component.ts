7176268cce2bce5f72e6bc4977fe15c8e9c739e4
Erkin <erkin1399@gmail.com>
2022-07-22 21:44:32 +0600

    Change vote button component

--- angular-front/src/app/shared/vote-button/vote-button.component.ts
+++ angular-front/src/app/shared/vote-button/vote-button.component.ts
@@ -1,6 +1,13 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {faArrowDown, faArrowUp } from '@fortawesome/free-solid-svg-icons';
 import { PostModel } from '../post-model';
+import {AuthService} from "../../auth/shared/auth.service";
+import {PostService} from "../post.service";
+import {throwError} from "rxjs";
+import {VotePayload} from "./vote-payload";
+import {ToastrService} from "ngx-toastr";
+import {VoteService} from "../vote.service";
+import {VoteType} from "./vote-type";
 
 @Component({
   selector: 'app-vote-button',
@@ -8,13 +15,54 @@ import { PostModel } from '../post-model';
   styleUrls: ['./vote-button.component.css']
 })
 export class VoteButtonComponent implements OnInit {
-  @Input() post: PostModel | undefined;
+  @Input() post: PostModel;
+  votePayload: VotePayload;
   faArrowUp = faArrowUp;
   faArrowDown = faArrowDown;
+  upvoteColor: string | undefined;
+  downvoteColor: string | undefined;
 
-  constructor() { }
+  constructor(private voteService: VoteService,
+              private authService: AuthService,
+              private postService: PostService, private toastr: ToastrService) {
+    // @ts-ignore
+    this.post = {}
 
-  ngOnInit(): void {
+    // @ts-ignore
+    this.votePayload = {}
   }
 
+  ngOnInit(): void {
+    this.updateVoteDetails();
+  }
+
+  upvotePost() {
+    this.votePayload.voteType = VoteType.UPVOTE;
+    this.vote();
+    this.downvoteColor = '';
+  }
+
+  downvotePost() {
+    this.votePayload.voteType = VoteType.DOWNVOTE;
+    this.vote();
+    this.upvoteColor = '';
+  }
+
+  private vote() {
+    // @ts-ignore
+    this.votePayload.postId = this.post.id;
+    this.voteService.vote(this.votePayload).subscribe(() => {
+      this.updateVoteDetails();
+    }, error => {
+      this.toastr.error(error.error.message);
+      throwError(error);
+    });
+  }
+
+  private updateVoteDetails() {
+    // @ts-ignore
+    this.postService.getPost(this.post.id).subscribe(post => {
+      this.post = post;
+    });
+  }
 }
